3548ee38f7d2cbdc8e0c4cce8be35c47bab9eced
wentan <wentan1995@gmail.com>
2021-11-28 20:57:51 +0800

    chore: merge

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,4 @@
-import imgUrl from '@/statics/img/placeholder_img.jpg';
-import { Layout, Drawer, Image, Typography, Card } from 'antd';
+import { Drawer, Typography } from 'antd';
 import React, { useState } from 'react';
 import Exhibition from '../../components/Exhibition';
 import ExhibitOverview from '../../components/ExhibitOverview';
@@ -8,30 +7,11 @@ import CustomeHeader from '../../components/Header';
 import Partner from '../../components/Partner';
 import ProductCatalog from "../../components/ProductCatalog";
 import SearchBar from '../../components/SearchBar';
-
 import { WorldMap } from './Map/';
 
-// Nation flags
-import flag_br from '@/statics/img/br.png';
-import flag_cn from '@/statics/img/cn.png';
-import flag_dz from '@/statics/img/dz.png';
-import flag_es from '@/statics/img/es.png';
-import flag_fr from '@/statics/img/fr.png';
-import flag_gr from '@/statics/img/gr.png';
-import flag_jp from '@/statics/img/jp.png';
-import flag_kr from '@/statics/img/kr.png';
-import flag_nl from '@/statics/img/nl.png';
-import flag_sg from '@/statics/img/sg.png';
-
-import NaviBar from "@/components/NaviBar/";
-
-const { Header, Footer, Content } = Layout;
-const { Meta } = Card;
 
 
 // 目前这些数据先写死，等后来服务端开发后，会从服务端获得数据
-const nationFlags = [ flag_br, flag_cn, flag_dz, flag_es, flag_fr, flag_gr, flag_jp, flag_kr, flag_nl, flag_sg ];
-const regions = [ '上海展区', '重庆展区', '北京展区', '陕西展区', '柬埔寨展区', '泰国展区', '香港展区', '澳大利亚展区', '美国展区', '俄罗斯展区' ];
 const { Text } = Typography;
 
 const HomePage = () => {
