7fc01120002dec6382fbb5937e705151833cd892
wbai95 <wbai95@qq.com>
2021-11-29 11:36:51 +0800

    feat: build config

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ const CustomeHeader = () => {
         <div>
             <Affix offsetTop={0}>
                 <div className="header">
-                    <Row justify="center" align="middle">
+                    <Row justify="center" align="middle" style={{ background: 'rgb(245, 245, 245)' }}>
                         <Col span={10}>
                             <img src={expo_logo} style={{ width: 330, height: 100 }} />
                         </Col>
